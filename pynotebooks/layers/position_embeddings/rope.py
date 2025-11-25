6123a98ec4b8212811c21b9e7ccd5aa2dbf92923
Delius <1092104983@qq.com>
2025-11-26 00:03:52 +0800

    update test of rope

---
+++ pynotebooks/layers/position_embeddings/rope.py
@@ -0,0 +1,117 @@
+"""
+implement RoPE, rotary position embeddings.
+Refer more to the following url:
+1. original paper: https://arxiv.org/pdf/2104.09864
+2. improved implementation: https://nn.labml.ai/transformers/rope/index.html
+"""
+
+import torch
+import torch.nn as nn
+
+from transformers.models.qwen2.modeling_qwen2 import Qwen2RotaryEmbedding
+
+from transformers.models.qwen2 import Qwen2Config
+
+my_llm_config = Qwen2Config(
+    vocab_size=151936,
+    hidden_size=512,
+    intermediate_size=4096*3,
+    num_attention_heads=16,
+    num_key_value_heads=4,
+    hidden_act="silu",
+    max_position_embeddings=32768,
+    use_cache=False,
+    tie_word_embeddings=False,
+    use_sliding_window=False,
+
+    attn_bias = False
+)
+
+class RoPE(nn.Module):
+    def __init__(self, d: int=128, base: int = 10_000):
+        """
+        * `d` is the number of features $d$
+        * `base` is the constant used for calculating $\Theta$
+        """
+        super().__init__()
+
+        self.base = base
+        self.d = d
+        self.cos_cached = None
+        self.sin_cached = None
+
+    def _build_cache(self, x: torch.Tensor):
+        """
+        Cache $\cos$ and $\sin$ values
+        """
+        # Return if cache is already built
+        if self.cos_cached is not None and x.shape[0] <= self.cos_cached.shape[0]:
+            return
+
+        # Get sequence length
+        seq_len = x.shape[0]
+
+        # $\Theta = {\theta_i = 10000^{-\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
+        theta = 1. / (self.base ** (torch.arange(0, self.d, 2).float() / self.d)).to(x.device)
+
+        # Create position indexes `[0, 1, ..., seq_len - 1]`
+        seq_idx = torch.arange(seq_len, device=x.device).float().to(x.device)
+
+        # Calculate the product of position index and $\theta_i$
+        idx_theta = torch.einsum('n,d->nd', seq_idx, theta)
+
+        # Concatenate so that for row $m$ we have
+        # $[m \theta_0, m \theta_1, ..., m \theta_{\frac{d}{2}}, m \theta_0, m \theta_1, ..., m \theta_{\frac{d}{2}}]$
+        idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
+
+        # Cache them
+        self.cos_cached = idx_theta2.cos()[:, None, None, :]
+        self.sin_cached = idx_theta2.sin()[:, None, None, :]
+
+    def _neg_half(self, x: torch.Tensor):
+        # $\frac{d}{2}$
+        d_2 = self.d // 2
+
+        # Calculate $[-x^{(\frac{d}{2} + 1)}, -x^{(\frac{d}{2} + 2)}, ..., -x^{(d)}, x^{(1)}, x^{(2)}, ..., x^{(\frac{d}{2})}]$
+        return torch.cat([-x[:, :, :, d_2:], x[:, :, :, :d_2]], dim=-1)
+
+    def forward(self, x: torch.Tensor):
+        """
+        * `x` is the Tensor at the head of a key or a query with shape `[seq_len, batch_size, n_heads, d]`
+        """
+        # Cache $\cos$ and $\sin$ values
+        self._build_cache(x)
+
+        # Sequence length
+        seq_len = x.shape[0]
+
+        # Split the features, we can choose to apply rotary embeddings only to a partial set of features.
+        x_rope, x_pass = x[..., :self.d], x[..., self.d:]
+
+        # Calculate
+        # $[-x^{(\frac{d}{2} + 1)}, -x^{(\frac{d}{2} + 2)}, ..., -x^{(d)}, x^{(1)}, x^{(2)}, ..., x^{(\frac{d}{2})}]$
+        neg_half_x = self._neg_half(x_rope)
+
+        # Calculate
+        #
+        # \begin{align}
+        # \begin{pmatrix}
+        # x^{(i)}_m \cos m \theta_i - x^{(i + \frac{d}{2})}_m \sin m \theta_i \\
+        # x^{(i + \frac{d}{2})}_m \cos m\theta_i + x^{(i)}_m \sin m \theta_i \\
+        # \end{pmatrix} \\
+        # \end{align}
+        #
+        # for $i \in {1, 2, ..., \frac{d}{2}}$
+        x_rope = (x_rope * self.cos_cached[:seq_len]) + (neg_half_x * self.sin_cached[:seq_len])
+
+        #
+        return torch.cat((x_rope, x_pass), dim=-1)
+
+
+def test():
+    # [seq_len, batch_size, n_heads, d]
+    hidden_states=torch.randint(0,100,size=(11, 1, 4, 128))
+    rope=RoPE()
+    output = rope(hidden_states)
+
+test()
