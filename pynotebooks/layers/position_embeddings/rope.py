3d790bb8737714e1b6c4d3e47983a23dfeff095e
Delius <1092104983@qq.com>
2025-12-02 00:11:22 +0800

    update rope implementation; not test

--- pynotebooks/layers/position_embeddings/rope.py
+++ pynotebooks/layers/position_embeddings/rope.py
@@ -3,6 +3,7 @@ implement RoPE, rotary position embeddings.
 Refer more to the following url:
 1. original paper: https://arxiv.org/pdf/2104.09864
 2. improved implementation: https://nn.labml.ai/transformers/rope/index.html
+3. different implementation styles brief: https://anyinlover.github.io/blog/deep_learning/deepseek_rope
 """
 
 import torch
@@ -27,91 +28,85 @@ my_llm_config = Qwen2Config(
     attn_bias = False
 )
 
-class RoPE(nn.Module):
-    def __init__(self, d: int=128, base: int = 10_000):
-        """
-        * `d` is the number of features $d$
-        * `base` is the constant used for calculating $\Theta$
-        """
+
+class BaseRoPE(nn.Module):
+    def __init__(self, llm_config:Qwen2Config):
         super().__init__()
+        self.head_dim = getattr(llm_config, "head_dim", llm_config.hidden_size // llm_config.num_attention_heads)
+        self.max_position_embeddings = llm_config.max_position_embeddings
+        self.rope_theta = llm_config.rope_theta
+        self._get_position_embeddings()
 
-        self.base = base
-        self.d = d
-        self.cos_cached = None
-        self.sin_cached = None
-
-    def _build_cache(self, x: torch.Tensor):
+    def _get_position_embeddings(self):
         """
-        Cache $\cos$ and $\sin$ values
+        calculate inverse frequencies:
+        $$
+        \\frac{1} \\
+        {rope_theta^{\\frac{2(i-1)}{d}}},
+        i \\in [1, 2, ..., head_dim//2]
+        $$
         """
-        # Return if cache is already built
-        if self.cos_cached is not None and x.shape[0] <= self.cos_cached.shape[0]:
-            return
+        inv_freq = torch.pow(self.rope_theta, torch.arange(0, self.head_dim//2, dtype=torch.int32)*2//self.head_dim)
+        self.inv_freq = torch.pow(inv_freq.float(torch.float32), -1)
+        self.register_buffer("inv_freq", self.inv_freq)
 
-        # Get sequence length
-        seq_len = x.shape[0]
-
-        # $\Theta = {\theta_i = 10000^{-\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
-        theta = 1. / (self.base ** (torch.arange(0, self.d, 2).float() / self.d)).to(x.device)
-
-        # Create position indexes `[0, 1, ..., seq_len - 1]`
-        seq_idx = torch.arange(seq_len, device=x.device).float().to(x.device)
-
-        # Calculate the product of position index and $\theta_i$
-        idx_theta = torch.einsum('n,d->nd', seq_idx, theta)
-
-        # Concatenate so that for row $m$ we have
-        # $[m \theta_0, m \theta_1, ..., m \theta_{\frac{d}{2}}, m \theta_0, m \theta_1, ..., m \theta_{\frac{d}{2}}]$
-        idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
-
-        # Cache them
-        self.cos_cached = idx_theta2.cos()[:, None, None, :]
-        self.sin_cached = idx_theta2.sin()[:, None, None, :]
-
-    def _neg_half(self, x: torch.Tensor):
-        # $\frac{d}{2}$
-        d_2 = self.d // 2
-
-        # Calculate $[-x^{(\frac{d}{2} + 1)}, -x^{(\frac{d}{2} + 2)}, ..., -x^{(d)}, x^{(1)}, x^{(2)}, ..., x^{(\frac{d}{2})}]$
-        return torch.cat([-x[:, :, :, d_2:], x[:, :, :, :d_2]], dim=-1)
-
-    def forward(self, x: torch.Tensor):
+    def forward(self, hidden_states:torch.Tensor):
         """
-        * `x` is the Tensor at the head of a key or a query with shape `[seq_len, batch_size, n_heads, d]`
+        construct:
+        1. cos[
+        [pos_i\*theta_1, pos_i\*theta_2, ..., pos_i\*theta_{d/2}, pos_i\*theta_1, pos_i\*theta_2, ..., pos_i\*theta_{d/2}]
+        , ...],
+
+        2. sin[
+        [pos_i\*theta_1, pos_i\*theta_2, ..., pos_i\*theta_{d/2}, pos_i\*theta_1, pos_i\*theta_2, ..., pos_i\*theta_{d/2}]
+        , ...],
+
+        pos_i \\in [0, 1, 2, ..., seq_len]
         """
-        # Cache $\cos$ and $\sin$ values
-        self._build_cache(x)
+        bsz, seq_len, _ = hidden_states.shape
+        
+        # expand inverse frequencies to [bsz, 1, head_dim]
+        expanded_inv_freq = self.inv_freq[None, None, :].expand(*[bsz, 1, -1])
+        # expand position ids to [bsz, seq_len, 1]
+        expanded_position_ids = torch.arange(
+            seq_len)[None, :, None].expand(*[bsz, -1, 1])
 
-        # Sequence length
-        seq_len = x.shape[0]
+        with torch.autocast(device_type=hidden_states.device, dtype=torch.float32):
+            #NOTE you don't need to manually convert tensor dtype to float32 and device to the same here,
+            # torch.autocast does these conversions automatically.
+            half_idx_theta = expanded_position_ids @ expanded_inv_freq
+            idx_theta = torch.cat([half_idx_theta, half_idx_theta], dim=-1)
+        
+        cos = idx_theta.cos()
+        sin = idx_theta.sin()
+        return cos, sin
 
-        # Split the features, we can choose to apply rotary embeddings only to a partial set of features.
-        x_rope, x_pass = x[..., :self.d], x[..., self.d:]
+    @classmethod
+    def apply_position_embeddings(cls, head_states:torch.Tensor, position_embeddings: tuple[torch.Tensor,torch.Tensor]):
+        """
+        calculate:
+        [
+        [cos pos_i \* theta, -sin pos_i \* theta],
+        [sin pos_i \* theta, cos pos_i \* theta]
+        ]
 
-        # Calculate
-        # $[-x^{(\frac{d}{2} + 1)}, -x^{(\frac{d}{2} + 2)}, ..., -x^{(d)}, x^{(1)}, x^{(2)}, ..., x^{(\frac{d}{2})}]$
-        neg_half_x = self._neg_half(x_rope)
+        @
 
-        # Calculate
-        #
-        # \begin{align}
-        # \begin{pmatrix}
-        # x^{(i)}_m \cos m \theta_i - x^{(i + \frac{d}{2})}_m \sin m \theta_i \\
-        # x^{(i + \frac{d}{2})}_m \cos m\theta_i + x^{(i)}_m \sin m \theta_i \\
-        # \end{pmatrix} \\
-        # \end{align}
-        #
-        # for $i \in {1, 2, ..., \frac{d}{2}}$
-        x_rope = (x_rope * self.cos_cached[:seq_len]) + (neg_half_x * self.sin_cached[:seq_len])
+        [
+        x^{(pos_i)},
+        -x^{(pos_i + head_dim//2)}
+        ]
+        """
+        bsz, attn_heads, seq_len, head_dim = head_states
+        cos, sin = position_embeddings
+        expanded_cos = cos[:, None, :, :].expand(*[-1, attn_heads, -1, -1])        
+        expaned_sin = sin[:, None, :, :].expand(*[-1, head_dim, -1, -1])
 
-        #
-        return torch.cat((x_rope, x_pass), dim=-1)
+        apply_cos = head_states * expanded_cos
+        
+        def rotate_half(x:torch.Tensor):
+            return torch.cat([-x[:, head_dim//2:], x[:, :head_dim//2]], dim=-1)
 
+        apply_sin = rotate_half(head_states) * expaned_sin
 
-def test():
-    # [seq_len, batch_size, n_heads, d]
-    hidden_states=torch.randint(0,100,size=(11, 1, 4, 128))
-    rope=RoPE()
-    output = rope(hidden_states)
-
-test()
+        return apply_cos+apply_sin
